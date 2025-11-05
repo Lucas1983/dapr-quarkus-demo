b6cdeceff47e39c5f5ae5b7ae4513b446026d03d
Lukasz Mielczarek <mielczarek.lukasz.karol@gmail.com>
2025-11-05 17:51:49 +0100

    Remove unused imports

--- customer-service/src/native-test/java/com/dapr/customer/CustomerControllerIT.java
+++
@@ -1,8 +0,0 @@
-package com.dapr.customer;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class CustomerControllerIT extends CustomerControllerTest {
-    // Execute the same tests but in packaged mode.
-}
--- customer-service/src/test/java/com/dapr/customer/CustomerControllerTest.java
+++
@@ -1,6 +0,0 @@
-package com.dapr.customer;
-
-import io.quarkus.test.junit.QuarkusTest;
-
-@QuarkusTest
-class CustomerControllerTest {}
